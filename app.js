006a323782d1296c8be856ff2086011f7a3f741a
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-11-12 17:42:29 +0000

    Commit final

--- app.js
+++ app.js
@@ -6,4 +6,19 @@ const sum = (a, b) => {
 
 console.log(sum(7, 3))
 
-module.exports = { sum };
+let fromDollarToYen = (dollar) => {
+    return((dollar / 1.2) * 127.9);
+};
+
+let fromEuroToDollar = (euro) => {
+    let result = euro * 1.2;
+    return result
+};
+
+let fromYenToPound = (yen) => {
+    return ((yen / 127.9) * 0.8);
+};
+
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
+
